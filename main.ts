6a378a5cb9766d438ff15addabfffb9b7cddbba4
CherryKitten <git@cherrykitten.dev>
2022-10-25 13:36:49 +0200

    WIP: Kompletten Bildschirm für Karte nutzen

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import { setupHamburger } from "./ts/hamburger";
 import { initializeMap } from "./ts/map";
 import "@fontsource/ubuntu/latin.css";
 import { setupLegend } from "./ts/legend";
-import { setupEmergencyButton, setupResetButton } from "./ts/buttons";
+import { setupEmergencyButton, setupResetButton, setupHeaderButton } from "./ts/buttons";
 import { addGoatCounter } from "./ts/goatcounter";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	setupLegend();
 	setupEmergencyButton();
 	setupResetButton();
+	setupHeaderButton();
 
 	if (import.meta.env.VITE_QUEER_LEXIKON_PRIVATE === "true") {
 		addGoatCounter();
