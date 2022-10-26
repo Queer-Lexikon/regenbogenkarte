4cc9f8038689a6a33b3b0ee055d00159f21130f7
nachtjasmin <nachtjasmin@outlook.de>
2022-10-26 14:25:21 +0200

    fixup! feat: add second button which is better aligned

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import { setupInfoButton } from "./ts/info-menu";
 import { initializeMap } from "./ts/map";
 import "@fontsource/ubuntu/latin.css";
 import { setupLegend } from "./ts/legend";
-import { setupEmergencyButton, setupResetButton, setupHeaderButton } from "./ts/buttons";
+import { setupEmergencyButton, setupResetButton, setupHamburgerButton } from "./ts/buttons";
 import { addGoatCounter } from "./ts/goatcounter";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	setupLegend();
 	setupEmergencyButton();
 	setupResetButton();
-	setupHeaderButton();
+	setupHamburgerButton();
 
 	if (import.meta.env.VITE_QUEER_LEXIKON_PRIVATE === "true") {
 		addGoatCounter();
