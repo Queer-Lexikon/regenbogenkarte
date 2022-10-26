93751b6c058f9adff5abefedfa0329cb9f5ec4fb
nachtjasmin <nachtjasmin@outlook.de>
2022-10-26 14:25:37 +0200

    test: add hamburger button tests

--- tests/start.spec.ts
+++ tests/start.spec.ts
@@ -21,3 +21,35 @@ test.describe("exit button", () => {
 		await expect(page.context().pages()).toHaveLength(2);
 	});
 });
+test.describe("hamburger button", () => {
+	// Use a Pixel 5 as a reference mobile device.
+	test.use({
+		viewport: { width: 393, height: 851 },
+		deviceScaleFactor: 2,
+		hasTouch: true,
+	});
+
+	const buttonLocator = ".js-header-button:visible";
+
+	test.beforeEach(async ({ page, baseURL }) => {
+		test.fail(baseURL === undefined, "baseURL should be set");
+		await page.goto(baseURL!);
+	});
+
+	test("should be visible", async ({ page }) => {
+		await expect(page.locator(buttonLocator)).toBeVisible();
+	});
+	test("click reveals and unreveals menu", async ({ page }) => {
+		page.locator(buttonLocator).click();
+		await expect(page.locator("#mobile-header")).toBeVisible();
+
+		page.locator(buttonLocator).click();
+		await expect(page.locator("#mobile-header")).toBeHidden();
+	});
+	test("info menu button is not affected", async ({ page }) => {
+		const headerBtn = page.locator(buttonLocator);
+		headerBtn.click();
+		await page.locator("#open-menu-btn").tap();
+		await expect(page.locator("#menu >> .z-menu")).toBeVisible();
+	});
+});
