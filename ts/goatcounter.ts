7e77feca70783da0050e346f20c3b2e9c8db9049
xenein <76600392+xenein@users.noreply.github.com>
2024-03-08 19:22:19 +0100

    switch from goatcounter to umami

--- ts/goatcounter.ts
+++ ts/goatcounter.ts
@@ -14,9 +14,9 @@ export const addGoatCounter = () => {
 	};
 
 	let goatcounter = document.createElement("script");
-	goatcounter.src = "//ziege.queer-lexikon.net/count.js";
-	goatcounter.async = true;
-	goatcounter.setAttribute("data-goatcounter", "https://ziege.queer-lexikon.net/count");
+	goatcounter.src = "//glutamat.queer-lexikon.net/script.js";
+	goatcounter.defer = true;
+	goatcounter.setAttribute("data-website-id", "6e91c72c-29ef-4464-92b8-1a7d780bec01");
 
 	document.head.appendChild(goatcounter);
 };
